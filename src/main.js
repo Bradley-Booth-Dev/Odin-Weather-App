637b3657d4c78fa370896709c8a3265c292d2b86
Bradley Booth <bradley.booth@hotmail.co.uk>
2024-01-30 12:08:42 +0000

    Finished display weather function

--- src/main.js
+++ src/main.js
@@ -37,4 +37,35 @@ function displayWeather(data) {
     const weatherIcon = document.getElementById('weather-icon');
     const hourlyForecastDiv = document.getElementById('hourly-forecast');
 
+     // Clear previous content
+     weatherInfoDiv.innerHTML = '';
+     hourlyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';
+    
+    if (data.cod === '404') {
+        weatherInfoDiv.innerHTML = `<p>${data.message}<p>`
+    } else {
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15); // Convert to Celsius
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const temperatureHTML =  `
+        <p>${temperature}°C</p>
+    `;
+        const weatherHTML = `
+        <p>${cityName}<p>
+        <p>${description}<p>
+        `;
+
+        tempDivInfo.innerHTML = temperatureHTML
+        weatherInfoDiv = weatherHTML
+        weatherIcon.src = iconUrl
+        weatherIcon.alt = description
+
+        showImage();
+    }
+
 }
+function displayHourlyForecast(hourlyData)
